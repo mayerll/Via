0ebf7509a81a14784b278518827bad2d82fcda25
L.L <mayerlbp@hotmail.com>
2021-09-26 00:48:58 -0400

    Update listExeFile.py

--- listExeFile.py
+++ listExeFile.py
@@ -1,1 +1,12 @@
 # List all the execlusive files in a given dir (Python)
+
+import glob,os
+
+path = '/home/data_analysis/netflix'
+
+files = os.listdir(path)
+
+for f in files:
+  if f.lower().endswith('*.exe'):
+	print(f)
+   
